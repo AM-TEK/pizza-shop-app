1e794fe2455985f9f386b9aa6fe85fa588bdf6b7
Alejandro Maldonado <am@AM-WEBDEV.local>
2024-04-24 11:22:27 -0500

    implement useFetcher functionality

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,17 +1,26 @@
 /* eslint-disable react-refresh/only-export-components */
 // Test ID: IIDSAT
 import OrderItem from "./OrderItem"
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 
 function Order() {
   const order = useLoaderData()
+
+  const fetcher = useFetcher()
+
+  useEffect(function() {
+    if (!fetcher.data && fetcher.state === "idle")
+    fetcher.load('/menu')
+  }, [fetcher])
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -22,6 +31,7 @@ function Order() {
     estimatedDelivery,
     cart,
   } = order;
+  
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
   return (
@@ -52,7 +62,14 @@ function Order() {
 
       <ul className="border-t border-b divide-y divide-stone-200">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem 
+            item={item} 
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)?.ingredients ?? []
+            }
+         />
         ))}
       </ul>
 
@@ -61,6 +78,8 @@ function Order() {
         {priority && <p className="text-sm font-medium text-stone-600">Price priority: {formatCurrency(priorityPrice)}</p>}
         <p className="font-bold">To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
